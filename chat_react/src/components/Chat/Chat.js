46b1f81ae1c0ee4029b0961369d1d990680aa326
Sonu Babu T <111742766+Sonubabut@users.noreply.github.com>
2024-05-18 00:10:45 +0530

    Update Chat.js
    Removed the utils API

--- chat_react/src/components/Chat/Chat.js
+++ chat_react/src/components/Chat/Chat.js
@@ -3,7 +3,6 @@ import axios from "axios";
 import { makeStyles } from "@material-ui/core/styles";
 import TextField from "@material-ui/core/TextField";
 import Button from "@material-ui/core/Button";
-import { API_URL } from "../../utils/apiConfig";
 import "./Chat.css";
 
 const useStyles = makeStyles((theme) => ({
@@ -90,7 +89,7 @@ const Chat = () => {
       { role: "user", content: inputValue },
     ]);
     try {
-      const response = await axios.post(`"${API_URL}/api/get-response"`, {
+      const response = await axios.post("/api/get-response", {
         question: inputValue,
       });
 
